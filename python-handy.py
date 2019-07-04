20bc6f922413a7f4688fbaea70e6d36e97465472
Roshan Patel <34209643+rosh11090@users.noreply.github.com>
2019-07-04 16:49:16 +0400

    parellel function call added.

--- python-handy.py
+++ python-handy.py
@@ -7,3 +7,12 @@ def igroup(iterable, n):
        chunk = tuple(itertools.islice(it, n))
        if not chunk: return
        yield chunk
+    
+def run_parallel(*fns):
+    def helper():
+        import concurrent.futures
+        with concurrent.futures.ThreadPoolExecutor(max_workers=len(fns)) as e:
+            fs = [e.submit(fn) for fn in fns]
+            for f in fs:
+                yield f.result()
+    return tuple(helper())
